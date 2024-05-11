c899e56d5489d5ea10ff9f304d2a37b63d1f2775
yusikerem <kerem.yky@gmail.com>
2024-05-11 13:03:46 +0300

    add register login delete update requests

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,110 @@
 import express from "express";
+import User from "../models/userSchema.js";
 
 const router = express.Router();
 
-// router.post('/',(req,res)=>{
+router.post("/register", async (req, res, next) => {
+  const { username, password, role } = req.body;
 
-// })
+  try {
+    if (!username || !password) {
+      res.status(404).json({
+        message: "Username or password is missing",
+      });
+    } else if (password.length < 6) {
+      res.status(404).json({
+        message: "Password less than 6 characters",
+      });
+    }
+    const isUsernameExist = await User.findOne({ username });
+    if (isUsernameExist) {
+      return res.status(409).json({
+        message: "Username already exists",
+      });
+    }
+    User.create({ username, password, role }).then((user) =>
+      res.status(200).json({
+        user,
+      })
+    );
+  } catch (err) {
+    res.status(401).json({
+      message: "User not successful created",
+      error: error.mesage,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
+    if (!user) {
+      res
+        .status(401)
+        .json({ message: "Login not succesful", error: "User not found" });
+    } else {
+      res.status(200).json({ message: "succesfully logged in." });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "An error occured",
+      error: error.message,
+    });
+  }
+});
+router.put("/update", async (req, res) => {
+  const { role, id } = req.body;
+  // Verifying if role and id is presnt
+  if (role && id) {
+    // Verifying if the value of role is admin
+    if (role === "admin") {
+      await User.findById(id).then((user) => {
+        if (user.role !== "admin") {
+          user.role = role;
+          user.save().then((user) => {
+            res.status(200).json({
+              messeage: "Update successful",
+              user,
+            });
+          });
+        } else {
+          res.status(400).json({ message: "User is already an Admin" });
+        }
+      });
+    } else {
+      res.status(400).json({
+        message: "Role is not admin",
+      });
+    }
+  } else {
+    res.status(400).json({ message: "Role or Id not present" });
+  }
+});
+
+router.delete("/deleteUser", async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const result = await User.deleteOne({ _id: id });
+
+    if (result.deletedCount > 0) {
+      return res.status(200).json({ message: "User successfully deleted" });
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+});
+router.get("/users", (req, res) => {
+  const users = User.find()
+    .select("username password role _id")
+    .then((users) => {
+      res.json({ users });
+    })
+    .catch((err) => console.log(err));
+});
+export default router;
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import router from "./routes/index.js";
 const port = process.env.PORT;
+mongoose.connect(process.env.MONGO_URI, {}).then(() => {
+  console.log("DB connected");
+});
+mongoose.connection.on("error", (error) => {
+  console.log("database connection error: ", error);
+});
 
 const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/auth", router);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
